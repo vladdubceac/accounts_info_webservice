486e875e8ee63f8e9e7f36b97bb71ed8760b8191
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-04-06 22:45:11 +0300

    Added integration springdoc-openapi-ui library for documentation

--- src/main/java/md/vlad/mobile/app/controller/AccountController.java
+++ src/main/java/md/vlad/mobile/app/controller/AccountController.java
@@ -1,5 +1,10 @@
 package md.vlad.mobile.app.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import md.vlad.mobile.app.entity.Account;
 import md.vlad.mobile.app.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @RestController
@@ -19,6 +25,13 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @Operation(summary = "Get data for all accounts")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Founded accounts data",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class)) }),
+            @ApiResponse(responseCode = "404", description = "No accounts found",
+                    content = @Content) })
     @GetMapping("/all")
     public List<Account> getAllAccounts(){
         return accountService.getAccountsFromFunction();
--- src/main/java/md/vlad/mobile/app/dao/AccountRepository.java
+++ src/main/java/md/vlad/mobile/app/dao/AccountRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     @Query(value = "select * from get_accounts()", nativeQuery = true)
     public List<Account> readAccountsFromFunction();
+
+    public Optional<Account> findAccountById(Long id);
+
+    public List<Account> findAccountsByAccountNumber(String accountNumber);
 }
